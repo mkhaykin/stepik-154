d4f8a32d4647c4b8e0c990e9caaa33075cc94afd
mikhail.khaykin <m.khaykin@outlook.com>
2021-12-06 12:22:01 +0300

    init

--- hello.py
+++ hello.py
@@ -4,8 +4,8 @@
 def hello(environ, start_response):
     query = environ['QUERY_STRING'].strip('/?').split('&')
     # query = '/?a=1&a=2&b=3'.strip('/?').split('&')
-    s = '\n'.join(query)
-    data = s.encode('utf-8')
+    # s = '\n'.join(query)
+    # data = s.encode('utf-8')
 
     status = '200 OK'
     response_headers = [
@@ -13,5 +13,5 @@ def hello(environ, start_response):
         ('Content-Length', str(len(data)))
     ]
     start_response(status, response_headers)
-    return data
+    return query
 
