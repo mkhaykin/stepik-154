1f665edd114af2d31aacce0733d548225df9c9ce
mikhail.khaykin <m.khaykin@outlook.com>
2021-12-06 15:27:54 +0300

    init

--- hello.py
+++ hello.py
@@ -3,13 +3,15 @@
 
 def hello(environ, start_response):
     query = environ['QUERY_STRING'].strip('/?').split('&')
-    data = [bytes(s + '\n', 'ascii') for s in query]
-
+    # data = [bytes(s + '\n', 'ascii') for s in query]
+    s = '\n'.join(query)
     status = '200 OK'
     response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
+        ('Content-type', 'text/plain')  # ,
+        # ('Content-Length', str(len(s)))
     ]
     start_response(status, response_headers)
-    return data
-
+    if s:
+        return [bytes(s, 'utf-8')]
+    else:
+        return [bytes('no data', 'utf-8')]
