45e222f14c54c92f7250bb06f666283c033b2744
mikhail.khaykin <m.khaykin@outlook.com>
2021-12-06 16:34:26 +0300

    init

--- hello.py
+++ hello.py
@@ -3,21 +3,18 @@
 
 def app(environ, start_response):
     query = environ['QUERY_STRING'].strip('/?').split('&')
-    # s = '\n'.join(query)
-    s = '\n'.join(['a=a', 'b=b'])
-    if not s:
-        s = 'no data'
+    s = '\n'.join(query)
 
     status = '200 OK'
     response_headers = [
-        ('Content-type', 'text/plain')
+        ('Content-type', 'text/plain'),
         ('Content-Length', str(len(s)))
     ]
     start_response(status, response_headers)
-    return iter([bytes(s, 'utf-8')])
+    return iter([s.encode('utf-8')])
 
 
-def test_ok(environ, start_response):
+def app_ok(environ, start_response):
     """Simplest possible application object"""
     data = b'Hello, World!\n'
     status = '200 OK'
