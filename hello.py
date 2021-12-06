27387304910d8c8a5c4ce6b0d8d2c8974aac0407
mikhail.khaykin <m.khaykin@outlook.com>
2021-12-06 15:43:06 +0300

    init

--- hello.py
+++ hello.py
@@ -12,6 +12,17 @@ def hello(environ, start_response):
     ]
     start_response(status, response_headers)
     if s:
-        return [bytes(s, 'utf-8')]
+        return iter([bytes(s, 'utf-8')])
     else:
-        return [bytes('no data', 'utf-8')]
+        return item([bytes('no data', 'utf-8')])
+
+def app(environ, start_response):
+    """Simplest possible application object"""
+    data = b'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
