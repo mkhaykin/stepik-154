68b1e0cea012658c93088f4aafc1c70d64e8443a
mikhail.khaykin <m.khaykin@outlook.com>
2021-12-06 13:50:37 +0300

    init

--- copy.sh
+++ copy.sh
@@ -1,3 +1,7 @@
+mkdir /home/box/web
+mkdir /home/box/web/etc
+mkdir /home/box/etc
+
 cp /home/box/stepik-154/nginx.conf /home/box/web/etc/nginx.conf
 cp /home/box/stepik-154/hello.py /home/box/web/hello.py
 cp /home/box/stepik-154/gunicorn.conf /home/box/etc/gunicorn.conf
