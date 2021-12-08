6a453b412a648228e0762848d02df6b601a80dbc
mikhail.khaykin <m.khaykin@outlook.com>
2021-12-08 13:00:32 +0300

    init

--- copy.sh
+++ copy.sh
@@ -1,3 +1,4 @@
 cp /home/box/stepik-154/nginx.conf /home/box/web/etc/nginx.conf
 cp /home/box/stepik-154/hello.py /home/box/web/hello.py
 cp /home/box/stepik-154/gunicorn.conf /home/box/etc/gunicorn.conf
+cp /home/box/stepik-154/gunicorn_django.conf /home/box/etc/gunicorn_django.conf
