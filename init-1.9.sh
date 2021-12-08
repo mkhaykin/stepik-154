4a4e035bee0b5d4b8f54d2f353aa44ee0cd31345
mikhail.khaykin <m.khaykin@outlook.com>
2021-12-08 12:23:25 +0300

    init

--- init-1.9.sh
+++ init-1.9.sh
@@ -2,8 +2,6 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo ln -sf /home/box/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -sf /home/box/etc/gunicorn_django.conf /etc/gunicorn.d/gunicorn_django
-
 
 sudo /etc/init.d/nginx restart
 sudo service gunicorn restart
