aae59cbde99ac76f9bbd02de1ba2820cde6a572e
mikhail.khaykin <m.khaykin@outlook.com>
2021-12-13 09:37:42 +0300

    init

--- db.sh
+++ db.sh
@@ -1,6 +1,9 @@
 sudo /etc/init.d/mysql start
-mysql -uroot -e "create database stepic"
+mysql -uroot -e "create database stepic;"
+mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'nopass77';"
 mysql -uroot -e "grant all privileges on stepic.* to 'box'@'localhost' with grant option;"
+
+
 cd /home/box/web/ask/
 python manage.py makemigrations
 python manage.py migrate
